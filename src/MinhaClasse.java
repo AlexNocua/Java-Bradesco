8e4b2a81e903c3080428c713fe0a537d38c7a943
AlexNocua <nocua68@gmail.com>
2025-04-21 17:03:09 -0500

    Avances respecto a repaso de metodos y definicion de variables.

---
+++ src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+
+    // camel case para o nome das variavels
+    public int number;
+    
+    final String name = "Lucas"; // constante, não pode ser alterada
+    final String NAME = "Lucas"; // constante, não pode ser alterada
+
+    // metodo princial de execucao
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+    }
+}
---
+++ src/Variaveis_metodos.java
@@ -0,0 +1,15 @@
+public class Variaveis_metodos {
+    // exemplos de variaveis
+    int idade = 20;
+    int anoFabricacao = 2023;
+    boolean verdadeira = true;
+
+    // exemplos de metodos
+    public static int somar(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(somar(2, 3));
+    }
+}
